cb680a12afce6b38dab5a6e98bfe2c2cefeae891
nirtus <nirtulys@gmail.com>
2023-02-02 19:27:31 +0200

    initial commit

---
+++ Garage/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Garage;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
