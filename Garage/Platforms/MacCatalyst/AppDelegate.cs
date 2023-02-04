d4c70c4c97edbb73db4518c0fee40fcdcb5cf0db
nirtus <39859770+nirtus@users.noreply.github.com>
2023-02-04 10:57:58 +0200

    Delete Garage directory

--- Garage/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace Garage;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
